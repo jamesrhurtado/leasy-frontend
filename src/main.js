27b69249fbf519b6a0f73a8c76ad4726f31484e6
jrenzzo <u202118568@upc.edu.pe>
2022-10-11 16:57:30 -0500

    feat: implemented validation with regexp and functions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { Quasar } from 'quasar'
+import { Quasar, Notify } from 'quasar'
 
 import App from './App.vue'
 import router from './router'
@@ -13,7 +13,12 @@ import '@quasar/extras/material-icons/material-icons.css'
 const app = createApp(App)
 
 app.use(Quasar, {
-    plugins: {}, // import Quasar plugins and add here
+    plugins: {
+        Notify
+    },
+    config: {
+      notify: { /* look at QuasarConfOptions from the API card */ }
+    }
   })
 
 app.use(createPinia())
