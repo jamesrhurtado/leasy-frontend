55f26f6cf0891133b5541bc2ddc0def5eb04e62f
jrenzzo <u202118568@upc.edu.pe>
2022-10-12 16:57:16 -0500

    feat: prompt for default data and validation

--- src/accounts/pages/user-settings.component.vue
+++ src/accounts/pages/user-settings.component.vue
@@ -7,8 +7,8 @@ export default {
 
     function promptCurrency () {
       $q.dialog({
-        title: 'Cambiar moneda',
-        message: '¿Cuál es la nueva moneda que desea?',
+        title: 'Editar moneda',
+        message: 'Seleccione la moneda que desea',
         options: {
           type: 'radio',
           model: 'opt1',
@@ -25,8 +25,62 @@ export default {
       })
     }
 
+    function promptNDaysPerYear () {
+      $q.dialog({
+        title: 'Editar días por año',
+        message: 'Seleccione el numero de días por año',
+        options: {
+          type: 'radio',
+          model: 'opt1',
+          items: [
+            { label: '360 días', value: '360', color: 'secondary' },
+            { label: '365 días', value: '365', color: 'secondary'}
+          ]
+        },
+        cancel: true,
+        persistent: true
+      }).onOk(data => {
+        // console.log('>>>> OK, received', data)
+      })
+    }
+
+    function promptValueAddedTax () {
+        $q.dialog({
+          title: 'Editar IGV',
+          message: 'Escriba el nuevo valor (en %)',
+          prompt: {
+            model: '',
+            isValid: val => val > 0, // << here is the magic
+            type: 'number' // optional
+          },
+          cancel: true,
+          persistent: true
+        }).onOk(data => {
+          // console.log('>>>> OK, received', data)
+        })
+    }
+
+    function promptIncomeTax () {
+        $q.dialog({
+          title: 'Editar IR',
+          message: 'Escriba el nuevo valor (en %)',
+          prompt: {
+            model: '',
+            isValid: val => val > 0, // << here is the magic
+            type: 'number' // optional
+          },
+          cancel: true,
+          persistent: true
+        }).onOk(data => {
+          // console.log('>>>> OK, received', data)
+        })
+    }
+
     return {
-      promptCurrency
+      promptCurrency,
+      promptNDaysPerYear,
+      promptValueAddedTax,
+      promptIncomeTax
     }
   }
 }
@@ -40,8 +94,11 @@ export default {
         <q-input class="p-4 m-2" outlined v-model="currency" hint="Moneda" :dense="dense" readonly />
         <q-btn label="Editar" color="primary" @click="promptCurrency" />
         <q-input class="p-4 m-2" outlined v-model="nDaysPeryear" hint="Número de dias por año" :dense="dense" readonly />
+        <q-btn label="Editar" color="primary" @click="promptNDaysPerYear" />
         <q-input class="p-4 m-2 mb-6" outlined v-model="valueAddedTax" hint="Impuesto general a las ventas (IGV)" :dense="dense" readonly />
+        <q-btn label="Editar" color="primary" @click="promptValueAddedTax" />
         <q-input class="p-4 m-2 mb-6" outlined v-model="incomeTax" hint="Impuesto general a la renta (IR)" :dense="dense" readonly />
+        <q-btn label="Editar" color="primary" @click="promptIncomeTax" />
 
     </div>
   
