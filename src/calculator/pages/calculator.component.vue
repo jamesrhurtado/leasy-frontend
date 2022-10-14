e733ffb389d28f7aff162b530480ee822602b7dd
jrenzzo <u202118568@upc.edu.pe>
2022-10-14 12:48:06 -0500

    feat(calculator): Added component in router file

---
+++ src/calculator/pages/calculator.component.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        <div>Leasing</div>
+        <div>Ingrese los datos</div>
+        <q-separator />
+    </div>
+</template>
+
+<style>
+
+</style>
