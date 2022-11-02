2f0bb7d0f3ee8229126c61224db57d58393888b8
jrenzzo <u202118568@upc.edu.pe>
2022-11-01 21:23:29 -0500

    feat(server): Implemented Service files to use Fake API

---
+++ src/calculator/services/reports.service.js
@@ -0,0 +1,23 @@
+import http from '../../core/services/http-common';
+
+export class ReportsApiService{
+    getAll(){
+        return http.get("/reports")
+    }
+
+    getById(id){
+        return http.get(`/reports/${id}`)
+    }
+
+    create(data){
+        return http.post("/reports", data);
+    }
+
+    update(id, data){
+        return http.put(`/reports/${id}`, data)
+    }
+
+    delete(id){
+        return http.delete(`/reports/${id}`)
+    }
+}
