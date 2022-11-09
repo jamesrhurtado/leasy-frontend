8a97778ab19361f5c4b0d2886906f196bf62bb3e
jrenzzo <u202118568@upc.edu.pe>
2022-11-09 12:29:17 -0500

    refactor(settings): Changed from Options API to Composition API in Settings

--- src/accounts/pages/user-settings.component.vue
+++ src/accounts/pages/user-settings.component.vue
@@ -1,92 +1,81 @@
-<script>
+<script setup>
 import { useQuasar } from 'quasar'
 import { ref } from 'vue'
 
-export default {
-  setup () {
-    const $q = useQuasar()
-    const currency = ref("")
+const $q = useQuasar()
+const currency = ref("")
 
-    function promptCurrency () {
-      $q.dialog({
-        title: 'Editar moneda',
-        message: 'Seleccione la moneda que desea',
-        options: {
-          type: 'radio',
-          model: 'opt1',
-          items: [
-            { label: 'Dólar americano', value: 'Dólar americano', color: 'secondary' },
-            { label: 'Euro', value: 'Euro', color: 'secondary'},
-            { label: 'Sol peruano', value: 'Sol peruano', color: 'secondary' }
-          ]
-        },
-        cancel: true,
-        persistent: true
-      }).onOk(data => {
-        // console.log('>>>> OK, received', data)
-      })
-    }
-
-    function promptNDaysPerYear () {
-      $q.dialog({
-        title: 'Editar días por año',
-        message: 'Seleccione el numero de días por año',
-        options: {
-          type: 'radio',
-          model: 'opt1',
-          items: [
-            { label: '360 días', value: '360', color: 'secondary' },
-            { label: '365 días', value: '365', color: 'secondary'}
-          ]
-        },
-        cancel: true,
-        persistent: true
-      }).onOk(data => {
-        // console.log('>>>> OK, received', data)
-      })
-    }
-
-    function promptValueAddedTax () {
-        $q.dialog({
-          title: 'Editar IGV',
-          message: 'Escriba el nuevo valor (en %)',
-          prompt: {
-            model: '',
-            isValid: val => val > 0, // << here is the magic
-            type: 'number' // optional
-          },
-          cancel: true,
-          persistent: true
-        }).onOk(data => {
-          // console.log('>>>> OK, received', data)
-        })
-    }
-
-    function promptIncomeTax () {
-        $q.dialog({
-          title: 'Editar IR',
-          message: 'Escriba el nuevo valor (en %)',
-          prompt: {
-            model: '',
-            isValid: val => val > 0, // << here is the magic
-            type: 'number' // optional
-          },
-          cancel: true,
-          persistent: true
-        }).onOk(data => {
-          // console.log('>>>> OK, received', data)
-        })
-    }
-
-    return {
-        currency,
-        promptCurrency,
-        promptNDaysPerYear,
-        promptValueAddedTax,
-        promptIncomeTax
-    }
-  }
+function promptCurrency() {
+  $q.dialog({
+    title: 'Editar moneda',
+    message: 'Seleccione la moneda que desea',
+    options: {
+      type: 'radio',
+      model: 'opt1',
+      items: [
+        { label: 'Dólar americano', value: 'Dólar americano', color: 'secondary' },
+        { label: 'Euro', value: 'Euro', color: 'secondary' },
+        { label: 'Sol peruano', value: 'Sol peruano', color: 'secondary' }
+      ]
+    },
+    cancel: true,
+    persistent: true
+  }).onOk(data => {
+    // console.log('>>>> OK, received', data)
+  })
 }
+
+function promptNDaysPerYear() {
+  $q.dialog({
+    title: 'Editar días por año',
+    message: 'Seleccione el numero de días por año',
+    options: {
+      type: 'radio',
+      model: 'opt1',
+      items: [
+        { label: '360 días', value: '360', color: 'secondary' },
+        { label: '365 días', value: '365', color: 'secondary' }
+      ]
+    },
+    cancel: true,
+    persistent: true
+  }).onOk(data => {
+    // console.log('>>>> OK, received', data)
+  })
+}
+
+function promptValueAddedTax() {
+  $q.dialog({
+    title: 'Editar IGV',
+    message: 'Escriba el nuevo valor (en %)',
+    prompt: {
+      model: '',
+      isValid: val => val > 0, // << here is the magic
+      type: 'number' // optional
+    },
+    cancel: true,
+    persistent: true
+  }).onOk(data => {
+    // console.log('>>>> OK, received', data)
+  })
+}
+
+function promptIncomeTax() {
+  $q.dialog({
+    title: 'Editar IR',
+    message: 'Escriba el nuevo valor (en %)',
+    prompt: {
+      model: '',
+      isValid: val => val > 0, // << here is the magic
+      type: 'number' // optional
+    },
+    cancel: true,
+    persistent: true
+  }).onOk(data => {
+    // console.log('>>>> OK, received', data)
+  })
+}
+
 </script>
 
 <template>
